9f392d4c1d59bbbbaf4b31c9b2f1fe2aeff6dd6e
Naresh Rawat <nareshrawat33@gmail.com>
2020-09-21 14:37:21 +0545

    basic setup

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const ComponentName = () => {
+  return <View style={{flex: 1, backgroundColor: 'cyan'}} />;
+};
+export default ComponentName;
